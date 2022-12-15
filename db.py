d079dfb2f9f605ff9ec02e0ea3fc5b9eeb425010
Данил <106977864+PolitunDanil@users.noreply.github.com>
2022-12-15 21:38:30 +0200

    Delete db.py

--- db.py
+++
@@ -1,8 +0,0 @@
-import mysql.connector
-
-cnx = mysql.connector.connect(host='localhost',
-                              user='root',
-                              password='virb20zf4x',
-                              port='3306',
-                              database='for_poc3'
-                              )
